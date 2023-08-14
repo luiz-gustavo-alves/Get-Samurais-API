8d8328d501656fd866587abec12fcef414516a0c
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-08-14 12:50:38 -0300

    fix: dotenv in app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
-
 import router from "./routes/index.routes.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(express.json());
