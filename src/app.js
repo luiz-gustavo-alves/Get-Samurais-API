f2930313b214f8f45ccaecb3a80f1b5aa19f881e
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-08-14 12:48:31 -0300

    chore: adjust code for deploy on render

--- src/app.js
+++ src/app.js
@@ -9,5 +9,5 @@ app.use(cors());
 
 app.use(router);
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
--- src/database/db.connection.js
+++ src/database/db.connection.js
@@ -8,5 +8,7 @@ const configDatabase = {
     connectionString: process.env.DATABASE_URL,
 };
 
+if (process.env.NODE_ENV === "production") configDatabase.ssl = true;
+
 const db = new Pool(configDatabase);
 export default db;
