2b15a95bd73f1c9b0472e3af9de9f1f30285650b
Luiz Gustavo <luizgustavoalves702@gmail.com>
2023-12-10 23:47:41 -0300

    chore: console msg

--- src/app.js
+++ src/app.js
@@ -12,4 +12,4 @@ app.use(cors());
 app.use(router);
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
+app.listen(port, () => console.log(`Server running on port: ${port}`));
